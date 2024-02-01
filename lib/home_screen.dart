a192ff5d2f7450b7a17f480e3f1c93688c6c4748
Lucifer <singhanurudh570@gmail.com>
2024-02-01 11:20:23 +0530

    'created login form using bloc '

---
+++ lib/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_form_validation/bloc/auth_bloc.dart';
+import 'package:login_form_validation/login_screen.dart';
+import 'package:login_form_validation/widgets/gradient_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthInitial) {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const LoginScreen(),
+              ),
+              (route) => false,
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center(
+            child: Column(
+              children: [
+                Text((state as AuthSuccess).uid),
+                GradientButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(AuthLogoutRequested());
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
